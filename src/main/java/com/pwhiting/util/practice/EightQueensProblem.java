8885f66175462b5d8334c6bd4b4ea8b7b5fe6d24
Himself12794 <pwhiting.bba@gmail.com>
2015-09-14 18:36:42 -0400

    Solution for Eight Queen's Problem.
    It's not winning any awards for conciseness, but it appears to work.

--- src/main/java/com/pwhiting/util/practice/EightQueensProblem.java
+++ src/main/java/com/pwhiting/util/practice/EightQueensProblem.java
@@ -1,22 +1,34 @@
 package com.pwhiting.util.practice;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class EightQueensProblem {
 	
 	private static final Scanner SCANNER = new Scanner(System.in);
 	
-	private static ChessBoard readChessboard(Scanner sc) {
-		return new ChessBoard();
+	private static Chessboard readChessboard(Scanner sc) {
+		Chessboard board = new Chessboard();
+		
+		for (int i = 0; i < 8; i++) {
+			String row = sc.next();
+			board.readRow(row, i);
+		}
+		
+		return board;
+		
 	}
 	
 	public static void main(String[] args) {
 		
-		ChessBoard chessboard = readChessboard(SCANNER);	
-				
+		Chessboard chessboard = readChessboard(SCANNER);	
+		
+		System.out.println(chessboard);
+		System.out.println("Is valid? : " + chessboard.isValid());
+		
 	}
 	
-	private static class ChessBoard {
+	private static class Chessboard {
 		
 		private final boolean[][] board = new boolean[8][8];
 		
@@ -29,10 +41,92 @@ public class EightQueensProblem {
 				char[] chars = row.toCharArray();
 				
 				for (int i = 0; i < chars.length; i++) {
+					if (i >= board.length) break;
 					board[rowNum][i] = chars[i] == '*';
 				}
 				
 			}
+			
+		}
+		
+		private boolean checkAxes(int x, int y) {
+			
+			for (int y2 = 0; y2 < board.length; y2++) {
+				boolean value = board[x][y2];
+				if (y2 != y) {
+					if (value) return false;
+				}
+			}
+			
+			for (int x2 = 0; x2 < board.length; x2++) {
+				boolean value = board[x2][y];
+				if (x2 != x) {
+					if (value) return false;
+				}
+				
+			}
+			
+			return true;
+			
+		}
+		
+		private boolean checkNegSlope(int x, int y) {
+			
+			if (x >= 0 && y >= 0) {
+				int xT = x;
+				int yT = y;
+			
+				while (yT > 0 && xT > 0) {
+					xT--;
+					yT--;
+				}
+				
+				while (xT < board.length && yT < board.length) {
+					boolean value = board[xT][yT];
+					
+					if (xT != x && yT != y) {
+						if (value) return false;
+					}
+					
+					xT++;
+					yT++;
+				}
+				
+			} else {
+				throw new ArrayIndexOutOfBoundsException("Either x=" + x + " is out of bounds, or y=" + y + " is.");
+			}
+			
+			return true;
+			
+		}
+		
+		private boolean checkPosSlope(int x, int y) {
+			
+			if (x < board.length + 1 && y >= 0) {
+				int xT = x;
+				int yT = y;
+				
+				while (xT < board.length - 1 && yT > 0) {
+					xT++;
+					yT--;
+				}
+				
+				while (xT > 0 && yT < board.length - 2) {
+					boolean value = board[xT][yT];
+					
+					if (xT != x && yT != y) {
+						if (value) return false;
+					}
+					
+					xT--;
+					yT++;
+				}
+				
+			} else {
+				throw new ArrayIndexOutOfBoundsException("Either x=" + x + " is out of bounds, or y=" + y + " is.");
+			}
+			
+			return true;
 			
 		}
 		
@@ -42,11 +136,42 @@ public class EightQueensProblem {
 				
 				for (int j = 0; j < board[i].length; j++) {
 					
+					boolean value = board[i][j];
+					
+					if (value) {
+						boolean valid = true;
+						
+						valid &= checkAxes(i, j);
+						valid &= checkPosSlope(i, j);
+						valid &= checkNegSlope(i, j);
+						
+						if (valid) break;
+						else return false;
+					}
+					
 				}
 				
 			}
 			
-			return false;
+			return true;
+			
+		}
+		
+		public String toString() {
+			
+			StringBuilder value = new StringBuilder();
+			
+			for (int x = 0; x < board.length; x++) {
+				value.append('|');
+				for (int y = 0; y < board[x].length; y++) {
+					//value.append("(x=" + x + ",y=" + y + ",v=" + board[x][y]);
+					value.append(board[x][y] ? 'Q' : ' ');
+					value.append('|');
+				}
+				value.append("\n");
+			}
+			
+			return value.toString();
 			
 		}
 		
