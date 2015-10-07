ee21c544c1e0c9735ea2be00dc439b815f9e9e49
pwhiting <pwhiting.bba@gmail.com>
2015-10-07 13:03:31 -0400

    LimitedDataContainer2 now implements List

--- src/main/java/com/pwhiting/util/RangeLimitedDataContainer2.java
+++ src/main/java/com/pwhiting/util/RangeLimitedDataContainer2.java
@@ -1,18 +1,18 @@
 package com.pwhiting.util;
 
 import java.util.ArrayList;
+import java.util.Collection;
 import java.util.Iterator;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
 
 import com.google.common.collect.Lists;
 import com.google.common.collect.Range;
 
 /**
  * Represents a data structure whose data entries can be restricted to a certain
- * range. This class is recursive, so if {@code T} is also an instance of
- * DateLimitedDataContainer, it will check the date limit on that as well, and
- * so on. If desired, this can also work similarly to a list as long as all of
- * its entries implement {@link RangeLimitedData}
+ * range. Unlike {@link RangeLimitedDataContainer}, this is not recursive.
  *
  * @author phwhitin
  *
@@ -20,7 +20,7 @@ import com.google.common.collect.Range;
  * @param <C>
  */
 @SuppressWarnings({"rawtypes","unchecked"})
-public class RangeLimitedDataContainer2<C extends Comparable> implements Iterable<C> {
+public class RangeLimitedDataContainer2<C extends Comparable> implements List<C> {
 
 	private final List<C> data;
 
@@ -83,11 +83,11 @@ public class RangeLimitedDataContainer2<C extends Comparable> implements Iterabl
 	 * @return the limited object
 	 */
 	public RangeLimitedDataContainer2<C> includeAll() {
-		for (final C t : data) {
+		/*for (final C t : data) {
 			if (t instanceof RangeLimitedDataContainer2) {
 				((RangeLimitedDataContainer2) t).limitToRange(Range.all());
 			}
-		}
+		}*/
 		limitedData = Lists.newArrayList();
 		dateRange = Range.all();
 		return this;
@@ -136,6 +136,124 @@ public class RangeLimitedDataContainer2<C extends Comparable> implements Iterabl
 	
 	public int size() {
 		return isLimited() ? limitedData.size() : data.size();
+	}
+	
+	/**
+	 * Called when data is added to check to add to limited data
+	 */
+	private void reassess() {
+		limitToRange(getRange());
+	}
+
+	@Override
+	public void add(int index, C element) {
+		data.add(index, element);
+		reassess();
+	}
+
+	@Override
+	public boolean addAll(Collection<? extends C> c) {
+		boolean v = data.addAll(c);
+		reassess();
+		return v;
+	}
+
+	@Override
+	public boolean addAll(int index, Collection<? extends C> c) {
+		boolean v = data.addAll(index, c);
+		reassess();
+		return v;
+	}
+
+	@Override
+	public void clear() {
+		data.clear();
+		limitedData.clear();
+	}
+
+	@Override
+	public boolean contains(Object o) {
+		return isLimited() ? limitedData.contains(o) : data.contains(o);
+	}
+
+	@Override
+	public boolean containsAll(Collection<?> c) {
+		return isLimited() ? limitedData.containsAll(c) : data.containsAll(c);
+	}
+
+	@Override
+	public int indexOf(Object o) {
+		return isLimited() ? limitedData.indexOf(o) : data.indexOf(o);
+	}
+
+	@Override
+	public boolean isEmpty() {
+		return isLimited() ? limitedData.isEmpty() : data.isEmpty();
+	}
+
+	@Override
+	public int lastIndexOf(Object o) {
+		return isLimited() ? limitedData.lastIndexOf(o) : data.lastIndexOf(o);
+	}
+
+	@Override
+	public ListIterator<C> listIterator() {
+		return isLimited() ? limitedData.listIterator() : data.listIterator();
+	}
+
+	@Override
+	public ListIterator<C> listIterator(int index) {
+		return isLimited() ? limitedData.listIterator(index) : data.listIterator(index);
+	}
+
+	@Override
+	public boolean remove(Object o) {
+		boolean v = data.remove(o);
+		reassess();
+		return v;
+	}
+
+	@Override
+	public C remove(int index) {
+		C c = data.remove(index);
+		reassess();
+		return c;
+	}
+
+	@Override
+	public boolean removeAll(Collection<?> c) {
+		boolean v = data.removeAll(c);
+		reassess();
+		return v;
+	}
+
+	@Override
+	public boolean retainAll(Collection<?> c) {
+		boolean v = data.retainAll(c);
+		reassess();
+		return v;
+	}
+
+	@Override
+	public C set(int index, C element) {
+		C v = data.set(index, element);
+		reassess();
+		return v;
+	}
+
+	@Override
+	public List<C> subList(int fromIndex, int toIndex) {
+		return isLimited() ? limitedData.subList(fromIndex, toIndex) : data.subList(fromIndex, toIndex);
+	}
+
+	@Override
+	public Object[] toArray() {
+		return isLimited() ? limitedData.toArray() : data.toArray();
+	}
+
+	@Override
+	public <T> T[] toArray(T[] a) {
+		return isLimited() ? limitedData.toArray(a) : data.toArray(a);
 	}
 
 }
