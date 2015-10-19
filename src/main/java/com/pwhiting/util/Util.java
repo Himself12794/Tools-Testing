0b386c1714d75d06e84b46a8dc294991ab49783d
pwhiting <pwhiting@STEC237-13.uncfsu.edu>
2015-10-19 11:09:38 -0400

    removed to merge

--- src/main/java/com/pwhiting/util/Util.java
+++ src/main/java/com/pwhiting/util/Util.java
@@ -11,11 +11,11 @@ import java.util.Map;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ch.qos.logback.classic.Level;
+
 import com.google.common.collect.BoundType;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Range;
-
-import ch.qos.logback.classic.Level;
 
 /**
  * Utilities for use throughout the rest of the Application
